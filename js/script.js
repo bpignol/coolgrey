49afb85279e4393472463c7f6066ec828f968076
bpignol <bene@Jans-Mac.local>
2018-03-07 12:10:20 +0100

    menu icon 1 and 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+
+// function to know when you should show the icon or words in the navbar
+
 function menuClick (){
-  var disp = document.getElementById('menu-SM').style.display;
+  var disp = document.getElementById('menu-words').style.display;
   console.log(disp);
-  document.getElementById('menu-SM').style.display = "flex";
+  // var disp is used to know the status of the case 1 or 2, see in console
+  document.getElementById('menu-words').style.display = "flex";
+    // case 0 : class row is set by default as flex, initial status before clicking
   if (disp == "flex") {
-    document.getElementById('menu-SM').style.display = "none";
+    document.getElementById('menu-words').style.display = "none";
+    document.getElementById('menu-icon').style.backgroundImage = "url(asset/archive/svgs/switch1.svg)";
+    // case 1 : dont show words + show icon 1
   } else {
-    document.getElementById('menu-SM').style.display = "flex";
+    document.getElementById('menu-words').style.display = "flex";
+    document.getElementById('menu-icon').style.backgroundImage = "url(asset/archive/svgs/switch2.svg)";
+    // case 2 : show words + show icon 2
   }
 }
